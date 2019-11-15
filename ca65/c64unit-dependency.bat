0799f7f420cf0d57cc92bac4c487fd6efa9cadd8
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2019-11-14 20:13:52 -0500

    Remove wrong directory.

--- ca65/c64unit-dependency.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-IF EXIST vendor\c64unit (
-    cd vendor\c64unit
-    git pull https://Commocore@bitbucket.org/Commocore/c64unit.git master
-    cd ..\..\
-) ELSE (
-    git clone --origin c64unit git@bitbucket.org:Commocore/c64unit vendor\c64unit
-)
-@echo on
